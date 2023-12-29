261622e2fb1b5cd3501b34b3696d4db6a11342c0
Vladislav <denvill104@gmail.com>
2023-12-29 09:39:51 +0300

    ready

--- loginScrypt.js
+++ loginScrypt.js
@@ -1,13 +1,44 @@
-const registerButton = document.getElementById('button-register')
-const logginButton = document.getElementById('button-loggin')
+const registerButton = document.getElementById("button-register");
+const logginButton = document.getElementById("button-loggin");
+const inputPassword = document.getElementById("input-password");
+const inputEmail = document.getElementById("input-email");
+const errorLabel = document.getElementById("error-label");
 
-const changePageRegister = () => { 
-  window.location.href = 'register/register.html';
+const LOGGIN_URL = "http://localhost:5000/auth/login";
+
+const changePageLoggin = async () => {
+  try {
+    const value = {
+      email: inputEmail.value,
+      password: inputPassword.value,
+    };
+    const res = await fetch(LOGGIN_URL, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(value),
+    });
+    if (res.ok) {
+      const { token, role } = await res.json();
+      localStorage.setItem("token", token);
+      if (role == "user") {
+        window.location.href = "../userMainPage/userMainPage.html";
+      } else {
+        window.location.href = "../adminMainPage/adminMainPage.html";
+      }
+    } else {
+      throw new Error();
+    }
+  } catch (e) {
+    console.log(e);
+    errorLabel.textContent = String("Неверные данные");
+  }
+};
+
+const changePageRegister = () => {
+  window.location.href = "register/register.html";
 }
 
-const changePageLoggin = () => { 
-  window.location.href = 'adminMainPage/adminMainPage.html';
-}
-
-registerButton.addEventListener('click', changePageRegister)
-logginButton.addEventListener('click', changePageLoggin)
+registerButton.addEventListener("click", changePageRegister);
+logginButton.addEventListener("click", changePageLoggin);
--- register/register.js
+++ register/register.js
@@ -1,7 +1,10 @@
-const registerButton = document.getElementById('button-register');
-const inputEmail = document.getElementById('exampleInputEmail1');
-const inputPassword = document.getElementById('exampleInputPassword1');
-const inputPasswordAdmin = document.getElementById('exampleInputPasswordAdmin');
+const registerButton = document.getElementById("button-register");
+const inputEmail = document.getElementById("exampleInputEmail1");
+const inputPassword = document.getElementById("exampleInputPassword1");
+const inputPasswordAdmin = document.getElementById("exampleInputPasswordAdmin");
+const errorLabel = document.getElementById("error-label");
+
+const REGISTER_URL = "http://localhost:5000/auth/registration";
 
 const changePage = async () => {
   try {
@@ -9,21 +12,30 @@ const changePage = async () => {
       email: inputEmail.value,
       password: inputPassword.value,
       role: inputPasswordAdmin.value,
-    }
-    const response = await fetch('http://localhost:5000/auth/registration', {
-      method: 'POST', 
+    };
+
+    const res = await fetch(REGISTER_URL, {
+      method: "POST",
       body: JSON.stringify(value),
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        "Content-type": "application/json; charset=UTF-8",
       },
-    })
-    console.log(response.json());
-    localStorage.setItem('data', 'vale');
-    window.location.href = '../userMainPage/userMainPage.html';
-  } catch(e) { 
-    alert(2);
+    });
+    if(res.ok) {
+    const { token, role } = await res.json();
+      localStorage.setItem("token", token);
+      if(role == 'user') {
+        window.location.href = "../userMainPage/userMainPage.html";
+      } else {
+        window.location.href = "../adminMainPage/adminMainPage.html";
+      }
+    } else {
+      throw new Error('Что-то пошло не так');
+    }
+  } catch (e) {
     console.log(e);
+    errorLabel.textContent = String(e.message);
   }
-}
+};
 
-registerButton.addEventListener('click', changePage)
+registerButton.addEventListener("click", changePage);
